6e8314b20b2425eb4730fd4e5225a7d642e1219e
Maksym Prokopov <maksym.prokopov@billie.io>
2022-02-14 09:35:01 +0100

    separate logback.xml config for production

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ MAINTAINER Maksym Prokopov <mprokopov@gmail.com>
 ENV APP it-service-sse-1.3.0-standalone.jar
 ADD target/$APP /it-service-sse/app.jar
 
+COPY config/logback.prod.xml /logback.xml
+
 EXPOSE 9292
 
-CMD ["java", "-jar", "/it-service-sse/app.jar"]
+CMD ["java", "-Dlogback.configurationFile=/logback.xml", "-jar", "/it-service-sse/app.jar"]
--- project.clj
+++ project.clj
@@ -21,10 +21,11 @@
                  [environ "1.1.0"]]
                  ;; [celtuce "0.1.1-SNAPSHOT"]]
   :min-lein-version "2.0.0"
-  :resource-paths ["config", "resources"]
+  ;; :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
   ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "it-service-sse.server/run-dev"]}
+                   :resource-paths ["config", "resources"]
                    :env {:redis-host "localhost"}
                    :dependencies [[io.pedestal/pedestal.service-tools "0.5.2"]]}
              :uberjar {:aot [it-service-sse.server]}}
